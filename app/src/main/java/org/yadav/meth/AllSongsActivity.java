3ab89db014bb7154b593bc696af8af87fd232c8e
Yadav106 <sahil.jp.yadav@gmail.com>
2022-12-19 14:52:29 +0530

    added allsongs and liked songs activities, will implement them later

---
+++ app/src/main/java/org/yadav/meth/AllSongsActivity.java
@@ -0,0 +1,14 @@
+package org.yadav.meth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_songs);
+    }
+}
--- app/src/main/java/org/yadav/meth/MainActivity.java
+++ app/src/main/java/org/yadav/meth/MainActivity.java
@@ -2,13 +2,37 @@ package org.yadav.meth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAllSongs, btnLikedSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAllSongs = findViewById(R.id.btnAllSongs);
+        btnLikedSongs = findViewById(R.id.btnLikedSongs);
+
+        btnAllSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllSongsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLikedSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LikedSongsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
