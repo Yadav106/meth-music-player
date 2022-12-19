808184c0d6585c1e5ecc8a40af0d86ed6b33553f
Yadav106 <sahil.jp.yadav@gmail.com>
2022-12-20 02:00:45 +0530

    implemented the recycler view, asked for storage permissions, and showed all songs via the recycler view

--- app/src/main/java/org/yadav/meth/Song.java
+++ app/src/main/java/org/yadav/meth/Song.java
@@ -1,4 +1,39 @@
 package org.yadav.meth;
 
+import android.net.Uri;
+
 public class Song {
+
+    // attributes
+    String title;
+    Uri uri;
+    Uri artworkUri;
+    int duration;
+
+    // constructors
+    public Song(String title, Uri uri, Uri artworkUri, int duration) {
+        this.title = title;
+        this.uri = uri;
+        this.artworkUri = artworkUri;
+        this.duration = duration;
+    }
+
+    // getters
+    public String getTitle() {
+        return title;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
+    public Uri getArtworkUri() {
+        return artworkUri;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+
 }
--- app/src/main/java/org/yadav/meth/SongAdapter.java
+++ app/src/main/java/org/yadav/meth/SongAdapter.java
@@ -1,4 +1,83 @@
 package org.yadav.meth;
 
-public class SongAdapter {
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    // define variables
+    Context context;
+    List<Song> songs;
+
+    // constructor
+    public SongAdapter(Context context, List<Song> songs) {
+        this.context = context;
+        this.songs = songs;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // inflate the view from the viewholder
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_list_item, parent, false);
+        return new SongViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        // bind the details of the song with the inflated view's attributes
+        Song song = songs.get(position);
+        SongViewHolder viewHolder = (SongViewHolder) holder;
+
+        // set values to views
+        viewHolder.titleHolder.setText(song.getTitle());
+        viewHolder.durationHolder.setText(String.valueOf(song.getDuration()/(1000*60) + ":" + (song.getDuration()/1000)%60));
+
+        // artwork
+        Uri artworkUri = song.getArtworkUri();
+
+        if (artworkUri != null){
+            viewHolder.artworkHolder.setImageURI(artworkUri);
+
+            if(viewHolder.artworkHolder.getDrawable() == null){
+                viewHolder.artworkHolder.setImageResource(R.mipmap.treble);
+            }
+        }
+
+        // on item click
+        viewHolder.itemView.setOnClickListener(view -> Toast.makeText(context, song.getTitle(), Toast.LENGTH_SHORT).show());
+    }
+
+    // ViewHolder
+    public static class SongViewHolder extends RecyclerView.ViewHolder{
+
+        // Define some variables to connect the list item attributes
+        ImageView artworkHolder;
+        TextView titleHolder, durationHolder;
+
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // connect the list item attributes
+            artworkHolder = itemView.findViewById(R.id.artworkView);
+            titleHolder = itemView.findViewById(R.id.titleView);
+            durationHolder = itemView.findViewById(R.id.durationView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
 }
